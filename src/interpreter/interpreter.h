e62b7aad3ec0bdc8b92d70e36f729d1afb82403b
rbrauner <rafalbrauner@gmail.com>
2018-01-31 20:49:06 +0100

    Change arguments in methods in Interpreter from template type to Instructions_set pointer

--- src/interpreter/interpreter.h
+++ src/interpreter/interpreter.h
@@ -1,14 +1,16 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
+#include "instruction.h"
+#include <memory>
+
 namespace interpreter {
 namespace interpreter {
-    template <typename T>
     class Interpreter {
     public:
-        virtual void add_instructions(T instructions) = 0;
+        virtual void add_instructions_set(std::shared_ptr<Instructions_set> instructions_set) = 0;
         virtual void interpret(int how_many) = 0;
-        virtual void interpret(T instructions) = 0;
+        virtual void interpret(std::shared_ptr<Instructions_set> instructions_set) = 0;
     };
 } // namespace interpreter
 } // namespace interpreter
