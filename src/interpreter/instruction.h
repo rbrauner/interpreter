8f93ee4a906d5eb03fe5bd9257e0529dd92a4a8f
rbrauner <rafalbrauner@gmail.com>
2018-01-31 18:34:36 +0100

    Create Pre_formatted_instructions_buffer and Instructions_buffer interfaces

--- src/interpreter/instruction.h
+++ src/interpreter/instruction.h
@@ -1,11 +1,21 @@
 #ifndef INSTRUCTION_H
 #define INSTRUCTION_H
 
+#include "buffer.h"
+
 namespace interpreter {
 namespace instruction {
     class Instruction {
     public:
         virtual void exectute() = 0;
+    };
+
+    template <typename T>
+    class Pre_formatted_instructions_buffer : public buffer::FIFO_buffer<T> {
+    };
+
+    template <typename T>
+    class Instructions_buffer : public buffer::FIFO_buffer<T> {
     };
 } // namespace instruction
 } // namespace interpreter
