fd2c615f06d3616a031a3da1b9f6bda82bb06eb8
rbrauner <rafalbrauner@gmail.com>
2018-01-31 18:36:31 +0100

    Move Interpreter to namespace interpreter::interpreter

--- src/interpreter/interpreter.h
+++ src/interpreter/interpreter.h
@@ -2,13 +2,15 @@
 #define INTERPRETER_H
 
 namespace interpreter {
-template <typename T>
-class Interpreter {
-public:
-    virtual void add_instructions(T instructions) = 0;
-    virtual void interpret(int how_many) = 0;
-    virtual void interpret(T instructions) = 0;
-};
+namespace interpreter {
+    template <typename T>
+    class Interpreter {
+    public:
+        virtual void add_instructions(T instructions) = 0;
+        virtual void interpret(int how_many) = 0;
+        virtual void interpret(T instructions) = 0;
+    };
+} // namespace interpreter
 } // namespace interpreter
 
 #endif // INTERPRETER_H
