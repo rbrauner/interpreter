b423c2a344aa092dffe8777c957d20fe6bb26eeb
rbrauner <rafalbrauner@gmail.com>
2018-01-31 21:25:44 +0100

    Create Instructions_set interface

--- src/interpreter/instructions.h
+++ src/interpreter/instructions.h
@@ -5,6 +5,20 @@
 
 namespace interpreter {
 namespace instructions {
+    template <typename T>
+    class Instructions_set {
+    public:
+        virtual void add_instruction(T instruction) = 0;
+        virtual void remove_instruction(int number) = 0;
+        virtual void replace(int from, int to, Instructions_set_pointer Instructions_set, T blank) = 0;
+        virtual void swap(int first, int second) = 0;
+        virtual int size() = 0;
+        virtual Instructions_set<T> get_instructions_set(int from, int to) = 0;
+
+    private:
+        using Instructions_set_pointer = std::shared_ptr<Instructions_set<T>>;
+    };
+
     class Instruction {
     public:
         virtual void exectute() = 0;
