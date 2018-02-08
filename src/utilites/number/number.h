cc4ae007493aeb6a081937113dd797c3e2d4821b
rbrauner <rafalbrauner@gmail.com>
2018-02-08 15:45:30 +0100

    Amend methods to set value and numeral system in Number interface

--- src/utilites/number/number.h
+++ src/utilites/number/number.h
@@ -9,11 +9,14 @@ namespace number {
 
 class Number {
 public:
+  Number(std::shared_ptr<Number> value);
   virtual ~Number() = 0;
   virtual std::string getValue() = 0;
-  virtual void setValue(std::string) = 0;
+  virtual void setValue(std::string value) = 0;
 
   virtual void setNumeralSystem(std::shared_ptr<Number> numeralSystem) final;
+  virtual void
+  setNumeralSystemAndValue(std::shared_ptr<Number> numeralSystem) final;
 };
 
 } // namespace number
