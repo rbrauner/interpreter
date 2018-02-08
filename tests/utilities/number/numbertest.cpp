940eea010bd297644b1408ef77af33219195cac7
rbrauner <rafalbrauner@gmail.com>
2018-02-08 18:42:31 +0100

    Create tests for constructors and destructors for number module

--- tests/utilities/number/numbertest.cpp
+++ tests/utilities/number/numbertest.cpp
@@ -8,3 +8,19 @@ NumberTest::~NumberTest() {
   dec->~Number();
   hex->~Number();
 }
+
+TEST_F(NumberTest, Constructors) {
+  EXPECT_EQ("111100", bin->getValue());
+  EXPECT_EQ("60", dec->getValue());
+  EXPECT_EQ("3c", hex->getValue());
+}
+
+TEST_F(NumberTest, Destructors) {
+  bin->~Number();
+  dec->~Number();
+  hex->~Number();
+
+  EXPECT_EQ("0", bin->getValue());
+  EXPECT_EQ("0", dec->getValue());
+  EXPECT_EQ("0", hex->getValue());
+}
