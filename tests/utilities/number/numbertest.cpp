e4c29aa84e0ff0b3b06c487eb187d51948036c02
rbrauner <rafalbrauner@gmail.com>
2018-02-08 18:55:04 +0100

    Create setters tests for number module

--- tests/utilities/number/numbertest.cpp
+++ tests/utilities/number/numbertest.cpp
@@ -24,3 +24,29 @@ TEST_F(NumberTest, Destructors) {
   EXPECT_EQ("0", dec->getValue());
   EXPECT_EQ("0", hex->getValue());
 }
+
+TEST_F(NumberTest, Setters) {
+  bin->setValue("00010110");
+  dec->setValue("0022");
+  hex->setValue("0016");
+
+  EXPECT_EQ("10110", bin->getValue());
+  EXPECT_EQ("22", dec->getValue());
+  EXPECT_EQ("16", hex->getValue());
+
+  bin->setValue("0b01011010");
+  dec->setValue("090");
+  hex->setValue("0x09C");
+
+  EXPECT_EQ("1011010", bin->getValue());
+  EXPECT_EQ("90", dec->getValue());
+  EXPECT_EQ("9c", hex->getValue());
+
+  bin->setValue("0x01110000");
+  dec->setValue("0x0112");
+  hex->setValue("0b070");
+
+  EXPECT_EQ("1110000", bin->getValue());
+  EXPECT_EQ("112", dec->getValue());
+  EXPECT_EQ("70", hex->getValue());
+}
