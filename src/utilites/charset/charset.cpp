e9bc80c1d1499091447bdd2a1691cc4080180562
rbrauner <rafalbrauner@gmail.com>
2018-02-13 14:44:24 +0100

    Create CharSet class implementation

--- src/utilites/charset/charset.cpp
+++ src/utilites/charset/charset.cpp
@@ -1,1 +1,26 @@
 #include "charset.h"
+#include <algorithm>
+
+using std::begin;
+using std::end;
+using std::find;
+using std::initializer_list;
+using std::string;
+using utilities::charset::CharSet;
+
+CharSet::CharSet(initializer_list<char> charSet) : m_charSet{charSet} {}
+
+CharSet::CharSet(string charSet) : m_charSet{charSet} {}
+
+void CharSet::setCharSet(initializer_list<char> charSet) {
+  m_charSet = charSet;
+}
+
+void CharSet::setCharSet(string charSet) { m_charSet = charSet; }
+
+bool CharSet::checkIfExistsInCharSet(const char character) {
+  if (find(m_charSet.begin(), m_charSet.end(), character) != m_charSet.end())
+    return true;
+  else
+    return false;
+}
--- src/utilites/charset/charset.h
+++ src/utilites/charset/charset.h
@@ -8,12 +8,15 @@ namespace charset {
 
 class CharSet {
 public:
-  CharSet(std::initializer_list<char> charSet) {}
-  CharSet(std::string charSet) {}
+  CharSet(std::initializer_list<char> charSet);
+  CharSet(std::string charSet);
 
-  void setCharSet(std::initializer_list<char> charSet) {}
-  void setCharSet(std::string charSet) {}
-  bool checkIfExistsInCharSet(char character) { return true; }
+  void setCharSet(std::initializer_list<char> charSet);
+  void setCharSet(std::string charSet);
+  bool checkIfExistsInCharSet(const char character);
+
+private:
+  std::string m_charSet;
 };
 
 } // namespace charset
