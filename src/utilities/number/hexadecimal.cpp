43e1705f2be45c1fcf3558ed49cf1bc763dcae2f
rbrauner <rafalbrauner@gmail.com>
2018-02-21 10:37:35 +0100

    Create Hexadecimal implementation

--- src/utilities/number/hexadecimal.cpp
+++ src/utilities/number/hexadecimal.cpp
@@ -1,1 +1,14 @@
 #include "hexadecimal.h"
+
+using std::string;
+using utilities::charset::CharSet;
+using utilities::number::Hexadecimal;
+
+Hexadecimal::Hexadecimal(std::string value)
+    : NumberImpl::NumberImpl{CharSet{"0123456789abcdef"}} {
+  setValue(value);
+}
+
+std::string Hexadecimal::getValue() { return NumberImpl::getValue(); }
+
+void Hexadecimal::setValue(std::string value) { NumberImpl::setValue(value); }
--- src/utilities/number/hexadecimal.h
+++ src/utilities/number/hexadecimal.h
@@ -5,7 +5,13 @@
 namespace utilities {
 namespace number {
 
-class Hexadecimal : public NumberImpl {};
+class Hexadecimal : public NumberImpl {
+public:
+  Hexadecimal(std::string value);
+
+  std::string getValue() override;
+  void setValue(std::string value) override;
+};
 
 using Hex = Hexadecimal;
 
