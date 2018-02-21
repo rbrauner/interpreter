bdf054a915ebd139592bbe07722fbf6d916f7d11
rbrauner <rafalbrauner@gmail.com>
2018-02-21 10:37:14 +0100

    Create Decimal implementation

--- src/utilities/number/decimal.cpp
+++ src/utilities/number/decimal.cpp
@@ -1,1 +1,19 @@
 #include "decimal.h"
+
+using std::string;
+using std::to_string;
+using utilities::charset::CharSet;
+using utilities::number::Decimal;
+
+Decimal::Decimal(string value) : NumberImpl::NumberImpl{CharSet{"0123456789"}} {
+  setValue(value);
+}
+
+Decimal::Decimal(size_t value) : NumberImpl::NumberImpl{CharSet{"0123456789"}} {
+  string svalue = to_string(value);
+  setValue(svalue);
+}
+
+string Decimal::getValue() { return NumberImpl::getValue(); }
+
+void Decimal::setValue(string value) { NumberImpl::setValue(value); }
--- src/utilities/number/decimal.h
+++ src/utilities/number/decimal.h
@@ -5,7 +5,14 @@
 namespace utilities {
 namespace number {
 
-class Decimal : public NumberImpl {};
+class Decimal : public NumberImpl {
+public:
+  Decimal(std::string value);
+  Decimal(size_t value);
+
+  std::string getValue() override;
+  void setValue(std::string value) override;
+};
 
 using Dec = Decimal;
 
