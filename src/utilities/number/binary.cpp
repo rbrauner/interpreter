123a043f56562aa049883f53fc55d10e6332210e
rbrauner <rafalbrauner@gmail.com>
2018-02-21 10:27:33 +0100

    Create Binary implementation

--- src/utilities/number/binary.cpp
+++ src/utilities/number/binary.cpp
@@ -1,1 +1,13 @@
 #include "binary.h"
+
+using std::string;
+using utilities::charset::CharSet;
+using utilities::number::Binary;
+
+Binary::Binary(string value) : NumberImpl::NumberImpl{CharSet{"01"}} {
+  setValue(value);
+}
+
+std::string Binary::getValue() { return NumberImpl::getValue(); }
+
+void Binary::setValue(std::string value) { NumberImpl::setValue(value); }
--- src/utilities/number/binary.h
+++ src/utilities/number/binary.h
@@ -5,7 +5,13 @@
 namespace utilities {
 namespace number {
 
-class Binary : public NumberImpl {};
+class Binary : public NumberImpl {
+public:
+  Binary(std::string value);
+
+  std::string getValue() override;
+  void setValue(std::string value) override;
+};
 
 using Bin = Binary;
 
