4ede188c780d5cbba2d0a6818c7b5bc91a0a6f13
rbrauner <rafalbrauner@gmail.com>
2018-02-21 10:13:19 +0100

    Create NumberImpl implementation

--- src/utilities/number/numberimpl.cpp
+++ src/utilities/number/numberimpl.cpp
@@ -1,1 +1,31 @@
 #include "numberimpl.h"
+
+#include "utilities/number/numberexceptions.h"
+
+using std::string;
+using utilities::number::NumberImpl;
+
+string NumberImpl::getValue() { return m_value; }
+
+void NumberImpl::setValue(string value) {
+  // remove first, useless characters
+  size_t i = 0;
+  for (auto x : value) {
+    if (m_charSet.checkIfExistsInCharSet(x) && x != '0') {
+      break;
+    }
+    i++;
+  }
+
+  value = value.substr(i);
+
+  // check if value is correct
+  for (auto x : value) {
+    if (!m_charSet.checkIfExistsInCharSet(x)) {
+      throw exception::NumberIsNotCorrect{};
+    }
+  }
+
+  // set value
+  m_value = value;
+}
--- src/utilities/number/numberimpl.h
+++ src/utilities/number/numberimpl.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "utilities/charset/charset.h"
 #include "utilities/number/number.h"
+#include <string>
 
 namespace utilities {
 namespace number {
@@ -10,6 +12,10 @@ public:
   ~NumberImpl() override = 0;
   std::string getValue() override = 0;
   void setValue(std::string value) override = 0;
+
+private:
+  std::string m_value;
+  utilities::charset::CharSet m_charSet;
 };
 
 } // namespace number
