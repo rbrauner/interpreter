7b78ecb6ec6e13273f82c697518fbd02c3d4e577
rbrauner <rafalbrauner@gmail.com>
2018-02-28 10:04:52 +0100

    Amend constructors in brainfuck Interpreter class

--- src/brainfuck/interpreter/interpreter.h
+++ src/brainfuck/interpreter/interpreter.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "interpreter/interpreter/interpreter.h"
+#include <iostream>
 
 namespace brainfuck {
 namespace interpreter {
@@ -15,6 +16,14 @@ public:
 
   Interpreter() {}
   Interpreter(InstructionsBufferReference instructions) {}
+
+  Interpreter(std::istream &istream, std::ostream &ostream = std::cout) {}
+  Interpreter(InstructionsBufferReference instructions, std::istream &istream,
+              std::ostream &ostream = std::cout) {}
+
+  Interpreter(std::ostream &ostream, std::istream &istream = std::cin) {}
+  Interpreter(InstructionsBufferReference instructions, std::ostream &ostream,
+              std::istream &istream = std::cin) {}
 
   void
   addInstructions(const InstructionsBufferReference instructions) override {}
