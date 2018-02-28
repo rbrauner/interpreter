0092c3a5e06aad8e8b9a3dde61a9aa6ff4e4afd8
rbrauner <rafalbrauner@gmail.com>
2018-02-28 10:37:45 +0100

    Amend brainfuck MemoryPointer class after previous changes

--- src/brainfuck/memory/memorypointer.h
+++ src/brainfuck/memory/memorypointer.h
@@ -1,23 +1,15 @@
 #pragma once
 
 #include "brainfuck/address/address.h"
-#include "brainfuck/address/position.h"
-#include "brainfuck/address/value.h"
 #include "interpreter/memory/memorypointer.h"
 
 namespace brainfuck {
 namespace memory {
 
-class MemoryPointer : public ::interpreter::memory::MemoryPointer {
+class MemoryPointer : public ::interpreter::memory::MemoryPointer<
+                          ::brainfuck::address::Address> {
 public:
-  using AddressPointer = std::shared_ptr<interpreter::address::Address>;
-  using ValuePointer = std::shared_ptr<interpreter::address::Value>;
-  using PositionPointer = std::shared_ptr<interpreter::address::Position>;
-
-  MemoryPointer(AddressPointer address) {}
-
-  ValuePointer getValue() override {}
-  PositionPointer getPosition() override {}
+  MemoryPointer() {}
 };
 
 } // namespace memory
