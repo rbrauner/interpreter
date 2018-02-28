19721ec870e73a220efcf4ef4f9cea9869e9d2aa
rbrauner <rafalbrauner@gmail.com>
2018-02-28 10:41:28 +0100

    Add default template to interpreter InstructionsBuffer interface

--- src/interpreter/instruction/instructionsbuffer.h
+++ src/interpreter/instruction/instructionsbuffer.h
@@ -1,11 +1,12 @@
 #pragma once
 
+#include "interpreter/instruction/instruction.h"
 #include "utility/buffer/buffer.h"
 
 namespace interpreter {
 namespace instruction {
 
-template <typename T>
+template <typename T = Instruction>
 class InstructionsBuffer : public utility::buffer::FifoBuffer<T> {};
 
 } // namespace instruction
