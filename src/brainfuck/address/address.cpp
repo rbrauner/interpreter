0a4f78c2bd614d5cb09b080964f4ab928abe87d1
rbrauner <rafalbrauner@gmail.com>
2018-03-02 22:34:15 +0100

    Implement brainfuck Address class

--- src/brainfuck/address/address.cpp
+++ src/brainfuck/address/address.cpp
@@ -1,1 +1,14 @@
 #include "address.h"
+
+using brainfuck::address::Address;
+using brainfuck::address::Position;
+using brainfuck::address::Value;
+
+Address::Address(Position position, Value value)
+    : m_position{position}, m_value{value} {}
+
+interpreter::address::Position Address::getPosition() { return m_position; }
+
+interpreter::address::Value Address::getValue() { return m_value; }
+
+void Address::setValue(interpreter::address::Value value) { m_value = value; }
--- src/brainfuck/address/address.h
+++ src/brainfuck/address/address.h
@@ -1,11 +1,24 @@
 #pragma once
 
+#include "brainfuck/address/position.h"
+#include "brainfuck/address/value.h"
 #include "interpreter/address/address.h"
 
 namespace brainfuck {
 namespace address {
 
-class Address : public ::interpreter::address::Address {};
+class Address : public ::interpreter::address::Address {
+public:
+  Address(Position position, Value value = "0");
+
+  interpreter::address::Position getPosition() override;
+  interpreter::address::Value getValue() override;
+  void setValue(interpreter::address::Value value) override;
+
+private:
+  Position m_position;
+  Value m_value;
+};
 
 } // namespace address
 } // namespace brainfuck
