545cc2b89d5dfbf1d713bfa176b97aa1e98fae86
rbrauner <rafalbrauner@gmail.com>
2018-03-02 22:19:55 +0100

    Update interpreter Address interface

--- src/interpreter/address/address.h
+++ src/interpreter/address/address.h
@@ -1,11 +1,18 @@
 #pragma once
 
+#include "interpreter/address/position.h"
+#include "interpreter/address/value.h"
+
 namespace interpreter {
 namespace address {
 
 class Address {
 public:
   virtual ~Address() = 0;
+
+  virtual Position getPosition();
+  virtual Value getValue();
+  virtual void setValue(Value value);
 };
 
 } // namespace address
