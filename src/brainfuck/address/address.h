3da95035a07eaf12b8f9d920120c6de75796fe83
rbrauner <rafalbrauner@gmail.com>
2018-03-02 23:06:54 +0100

    Amend Address constructor

--- src/brainfuck/address/address.h
+++ src/brainfuck/address/address.h
@@ -8,7 +8,7 @@ namespace address {
 
 class Address {
 public:
-  Address(Position position, Value value = "0");
+  Address(Position position, Value value = utility::number::Decimal{"0"});
 
   Position getPosition();
   Value getValue();
