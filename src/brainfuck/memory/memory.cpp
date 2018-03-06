a7b8f0fe019dd746ac552223ceedbdfecf3ee1aa
rbrauner <rafalbrauner@gmail.com>
2018-03-06 10:23:08 +0100

    Fix bugs with clear() method in Memory class

--- src/brainfuck/memory/memory.cpp
+++ src/brainfuck/memory/memory.cpp
@@ -1,13 +1,14 @@
 #include "memory.h"
 
 using brainfuck::address::Address;
+using brainfuck::address::Value;
 using brainfuck::memory::Memory;
 using std::uint32_t;
 
 Memory::Memory(uint32_t size) : m_memory{size, Address{alias::Decimal{"0"}}} {}
 
 void Memory::clear() {
-  for (auto x : m_memory) {
-    x.setValue(alias::Decimal{"0"});
+  for (auto &x : m_memory) {
+    x.setValue(Value{alias::Decimal{"0"}});
   }
 }
