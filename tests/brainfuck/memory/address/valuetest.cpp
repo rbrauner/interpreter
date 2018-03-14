5efdc7fcb48036727c4a98d7fd02596c6abbce4d
rbrauner <rafalbrauner@gmail.com>
2018-03-14 17:17:12 +0100

    Inicialize ValueTest fields

--- tests/brainfuck/memory/address/valuetest.cpp
+++ tests/brainfuck/memory/address/valuetest.cpp
@@ -1,1 +1,14 @@
 #include "valuetest.h"
+#include "alias/number.h"
+
+using brainfuck::memory::address::Value;
+using brainfuck::memory::address::ValueTest;
+
+ValueTest::ValueTest()
+    : binDefault{alias::Binary{"1010"}}, decDefault{alias::Decimal{10}},
+      hexDefault{alias::Hexadecimal{"a"}},
+      minusTenToMinusFive{alias::Decimal{-7}, alias::Decimal{-10},
+                          alias::Decimal{-5}},
+      minusFiveToTen{alias::Decimal{2}, alias::Decimal{-5}, alias::Decimal{10}},
+      fiveToTwentyFive{alias::Decimal{14}, alias::Decimal{5},
+                       alias::Decimal{25}} {}
