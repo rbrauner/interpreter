8124ea06c69b9ea12e04ff89ec8cfd58846e0544
rbrauner <rafalbrauner@gmail.com>
2018-03-14 20:21:08 +0100

    Create empty definitions of ValueTest

--- tests/brainfuck/memory/address/valuetest.cpp
+++ tests/brainfuck/memory/address/valuetest.cpp
@@ -12,3 +12,13 @@ ValueTest::ValueTest()
       minusFiveToTen{alias::Decimal{2}, alias::Decimal{-5}, alias::Decimal{10}},
       fiveToTwentyFive{alias::Decimal{14}, alias::Decimal{5},
                        alias::Decimal{25}} {}
+
+TEST_F(ValueTest, InitializeProperlyWithDefaultMinAndMax) {}
+
+TEST_F(ValueTest, InitializeProperlyWithCorrectMinAndMax) {}
+
+TEST_F(ValueTest, ReportsErrorDuringInitializationWithIncorrectMinAndMax) {}
+
+TEST_F(ValueTest, ReportsErrorDuringInitializationWithOutOfRageValue) {}
+
+TEST_F(ValueTest, GettersWork) {}
