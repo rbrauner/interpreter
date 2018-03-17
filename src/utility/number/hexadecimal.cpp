ea10d3e8a2a10b343c16c7d77cb09b9c99d2dfde
rbrauner <rafalbrauner@gmail.com>
2018-03-17 18:30:38 +0100

    Implement hexadecimal conversions

--- src/utility/number/hexadecimal.cpp
+++ src/utility/number/hexadecimal.cpp
@@ -1,7 +1,14 @@
 #include "hexadecimal.h"
+#include "utility/number/binary.h"
+#include "utility/number/decimal.h"
+#include <sstream>
 
+using std::hex;
 using std::string;
+using std::stringstream;
 using utility::charset::CharSet;
+using utility::number::Binary;
+using utility::number::Decimal;
 using utility::number::Hexadecimal;
 
 Hexadecimal::Hexadecimal(std::string value)
@@ -19,3 +26,19 @@ bool Hexadecimal::operator==(const Hexadecimal &h) const {
 std::string Hexadecimal::getValue() const { return NumberImpl::getValue(); }
 
 void Hexadecimal::setValue(std::string value) { NumberImpl::setValue(value); }
+
+Binary utility::number::hexadecimalToBinary(const Hexadecimal &hexadecimal) {
+  Decimal decimal = hexadecimalToDecimal(hexadecimal);
+  Binary binary = decimalToBinary(decimal);
+  return binary;
+}
+
+Decimal utility::number::hexadecimalToDecimal(const Hexadecimal &hexadecimal) {
+  int64_t decimalNumber;
+  std::stringstream ss;
+
+  ss << hexadecimal.getValue();
+  ss >> hex >> decimalNumber;
+
+  return Decimal{decimalNumber};
+}
