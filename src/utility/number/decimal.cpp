180e58af9a6dda3214e310b8ea59623a342d44b1
rbrauner <rafalbrauner@gmail.com>
2018-03-17 18:26:30 +0100

    Implement decimal conversions

--- src/utility/number/decimal.cpp
+++ src/utility/number/decimal.cpp
@@ -1,9 +1,17 @@
 #include "decimal.h"
+#include "utility/number/binary.h"
+#include "utility/number/hexadecimal.h"
+#include <math.h>
+#include <sstream>
 
+using std::hex;
 using std::string;
+using std::stringstream;
 using std::to_string;
 using utility::charset::CharSet;
+using utility::number::Binary;
 using utility::number::Decimal;
+using utility::number::Hexadecimal;
 
 Decimal::Decimal(string value) : NumberImpl::NumberImpl{CharSet{"0123456789"}} {
   setValue(value);
@@ -25,3 +33,29 @@ bool Decimal::operator==(const Decimal &d) const {
 string Decimal::getValue() const { return NumberImpl::getValue(); }
 
 void Decimal::setValue(string value) { NumberImpl::setValue(value); }
+
+Binary utility::number::decimalToBinary(const Decimal &decimal) {
+  string binary = "";
+  int64_t decNumber = stoi(decimal.getValue());
+  int64_t binNumber = 0;
+  int64_t remainder = 0;
+  int64_t i = 0;
+
+  while (decNumber != 0) {
+    remainder = decNumber % 2;
+    decNumber /= 2;
+    binNumber += remainder * pow(10, i);
+    i++;
+  }
+
+  binary = to_string(binNumber);
+  return Binary{binary};
+}
+
+Hexadecimal utility::number::decimalToHexadecimal(const Decimal &decimal) {
+  stringstream hexadecimal;
+
+  hexadecimal << hex << stoi(decimal.getValue());
+
+  return Hexadecimal{hexadecimal.str()};
+}
